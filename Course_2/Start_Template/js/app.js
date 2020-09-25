7fa28b41965341bbe81e1d7c57e83568b7c00712
PedroMoralesB <pmorales@userena.cl>
2020-09-25 18:02:48 -0300

    Templates for Assignments.

--- Course_2/Start_Template/js/app.js
+++ Course_2/Start_Template/js/app.js
@@ -2,8 +2,13 @@
     'use strict';
 
     angular.module('appName', [])
-        .controller('appController', function() {
-
-        });
-    
+           .controller('appController', appControllerFunction);
+        
+    appControllerFunction.$inject = ['$scope'];
+    function appControllerFunction ($scope) {
+        $scope.name = "Variable Data";
+        $scope.funcData = function () {
+        return "Function Return";
+        };
+    };    
 })();
